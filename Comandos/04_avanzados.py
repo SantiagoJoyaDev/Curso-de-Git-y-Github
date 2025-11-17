5bd118f3f9b3bea23376a2d611139735579db359
Santiago Joya <santiagojoyab@outlook.com>
2025-11-17 17:03:29 +0100

    Curso de Git y Github terminado

---
+++ Comandos/04_avanzados.py
@@ -0,0 +1,4 @@
+# ---------- COMANDOS AVANZADOS ----------
+
+# Para regresara a un commit en concreto de una rama y traerlo a la rama la cual se esta trabajando en el momento --> git cherry-pick "Identificador del commit en especifico que queremos traer"
+# Para regresar una rama hasta un punto concreto y sobrescribe el historial de cambios tambien entonces todo los que teniamos se reemplaza con lo de la rama a la cual se volvio --> git rebase "Nombre de la rama"
